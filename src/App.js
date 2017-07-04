32d949e936d1d0afaabe8030f49625ed41d43920
Stephen Ward <stephen.ward21@gmail.com>
2017-07-04 18:21:03 -0400

    init repo

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import Login from './components/login'
+
+class App extends Component {
+	render() {
+	 return (
+	   <div className="App">
+        <h1>Password Manager</h1>
+
+	
+	   </div>
+	);
+	}
+}
+
+export default App;
