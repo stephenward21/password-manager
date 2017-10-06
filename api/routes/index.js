f1413219d3731025e950a27e238383c2b38e594f
Stephen Ward <stephen.ward21@gmail.com>
2017-10-05 20:51:23 -0400

    register contd server error

--- api/routes/index.js
+++ api/routes/index.js
@@ -5,67 +5,71 @@ var config = require('./config');
 var bcrypt = require('bcrypt');
 var randToken = require('rand-token');
 
-var connect = mysql.createConnection({
+var connection = mysql.createConnection({
     host: config.host,
     user: config.user,
     password: config.password,
     database: config.database
 });
 
+connection.connect();
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/register', (req,res) =>{
+router.post('/register', (req, res) => {
+	console.log(req.body);
+
 	const name = req.body.name;
     const email = req.body.email;
     const address = req.body.address;
     const city = req.body.city;
     const state = req.body.state;
     const password = bcrypt.hashSync(req.body.password);
-    console.log(name)
+    
 
 
-    // const checkEmail = new Promise((resolve,reject)=>{
-    //     const checkEmailQuery = 'SELECT * FROM user WHERE email = ?';
-    //     connection.query(checkEmailQuery,[email], (error,results)=>{
-    //         if(error) throw error;
-    //         if(results.length > 0){
-    //             reject({msg: "userAlreadyExists"});
-    //         }else{
-    //             resolve();
-    //         }
-    //     })
-    // });
-    // checkEmail.then(
-    //     ()=>{
-    var insertIntoCust = "INSERT INTO customers (name, address, city, state) VALUES (?,?,?,?)";
-    connection.query(insertIntoCust,[name,address,city,state], (error,results)=>{
-        const newID = results.insertId;
-        var token = randToken.uid(40);
-        var insertQuery = 'INSERT INTO user (email, uid, password, token) VALUES (?,?,?,?)';
-        connection.query(insertQuery, [email, newID, password, token], (error2, results2)=>{
-             console.log(results2)
-            if(error2){
-                res.json({
-                    msg: error2
-                })
+    const checkEmail = new Promise((resolve,reject)=>{
+        const checkEmailQuery = 'SELECT * FROM user WHERE email = ?';
+        connection.query(checkEmailQuery,[email], (error,results)=>{
+            if(error) throw error;
+            if(results.length > 0){
+                reject({msg: "userAlreadyExists"});
             }else{
-                res.json({
-                    msg: 'userInserted',
-                    name: name,
-                    token: token
-                });
+                resolve();
             }
-        });
-    })
-//     }
-// ).catch(
-//     (error)=>{
-//         res.json(error)
-//     }
-// )
+        })
+    });
+    checkEmail.then(
+        ()=>{
+		    var insertIntoCust = "INSERT INTO customers (name, address, city, state) VALUES (?,?,?,?)";
+		    connection.query(insertIntoCust,[name,address,city,state], (error,results)=>{
+		        const newID = results.insertId;
+		        var token = randToken.uid(40);
+		        var insertQuery = 'INSERT INTO user (email, uid, password, token) VALUES (?,?,?,?)';
+		        connection.query(insertQuery, [email, newID, password, token], (error2, results2)=>{
+		             console.log(results2)
+		            if(error2){
+		                res.json({
+		                    msg: error2
+		                })
+		            }else{
+		                res.json({
+		                    msg: 'userInserted',
+		                    name: name,
+		                    token: token
+		                });
+		            }
+		        });
+		    })
+		    }
+		).catch(
+		    (error)=>{
+		        res.json(error)
+		    }
+		)
 });
 
 module.exports = router;
--- src/reducers/registerReducer.js
+++ src/reducers/registerReducer.js
@@ -2,7 +2,6 @@ export default function(state = [], action){
     if(action.type === 'REGISTER') {
         console.log(action.payload);
         return action.payload;
-        // different action for group to not wipe out user info
     }else{
         return state;
     }
